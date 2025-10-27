48e6f87ab511821def119cff6e35e7607fb41224
jrffruit518-enrich <jrffruit518@gmail.com>
2025-10-27 13:57:16 +0100

    feat: add ArrayHandle.java

---
+++ src/main/java/sprint1Tasca_S104Nivell2_6/ArrayHandle.java
@@ -0,0 +1,9 @@
+package sprint1Tasca_S104Nivell2_6;
+
+public class ArrayHandle {
+    public static int accessArray(int index) {
+        int [] array ={1,2,3};
+        return array [index];
+
+    }
+}
